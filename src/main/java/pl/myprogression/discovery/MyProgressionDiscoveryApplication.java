993320135a1877900f1e0801688d95b5c65ca525
KrystianKorbecki <krystian.korbecki36@interia.pl>
2021-09-24 18:49:25 +0200

    init commit

---
+++ src/main/java/pl/myprogression/discovery/MyProgressionDiscoveryApplication.java
@@ -0,0 +1,17 @@
+package pl.myprogression.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MyProgressionDiscoveryApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MyProgressionDiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/myprogression/discovery/MyProgressionDiscoveryApplicationTests.java
@@ -0,0 +1,15 @@
+package pl.myprogression.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProgressionDiscoveryApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
